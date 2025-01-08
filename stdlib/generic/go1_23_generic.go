6f970c95821ce85d4c6d3b8ddb3dbf7c9a2c58c9
pangdogs <pangdogs@gmail.com>
2025-01-08 23:37:16 +0800

    整理泛型代码

--- stdlib/generic/go1_23_generic.go
+++ stdlib/generic/go1_23_generic.go
@@ -7,17 +7,23 @@ import (
 	_ "embed"
 )
 
+//go:embed go1_23_cmp_cmp.go.txt
+var go1_23_cmp_cmp string
+
 //go:embed go1_23_maps_maps.go.txt
 var go1_23_maps_maps string
 
-//go:embed go1_23_slices_math_bits.go.txt
-var go1_23_slices_math_bits string
+//go:embed go1_23_iter_iter.go.txt
+var go1_23_iter_iter string
 
-//go:embed go1_23_slices_cmp.go.txt
-var go1_23_slices_cmp string
+//go:embed go1_23_slices_builtin.go.txt
+var go1_23_slices_builtin string
 
 //go:embed go1_23_slices_iter.go.txt
 var go1_23_slices_iter string
+
+//go:embed go1_23_slices_sort.go.txt
+var go1_23_slices_sort string
 
 //go:embed go1_23_slices_zsortanyfunc.go.txt
 var go1_23_slices_zsortanyfunc string
@@ -25,20 +31,18 @@ var go1_23_slices_zsortanyfunc string
 //go:embed go1_23_slices_zsortordered.go.txt
 var go1_23_slices_zsortordered string
 
-//go:embed go1_23_slices_sort.go.txt
-var go1_23_slices_sort string
-
 //go:embed go1_23_slices_slices.go.txt
 var go1_23_slices_slices string
 
 // Sources contains the list of generic packages source strings.
 var Sources = [...]string{
+	go1_23_cmp_cmp,
 	go1_23_maps_maps,
-	go1_23_slices_math_bits,
-	go1_23_slices_cmp,
+	go1_23_iter_iter,
+	go1_23_slices_builtin,
 	go1_23_slices_iter,
+	go1_23_slices_sort,
 	go1_23_slices_zsortanyfunc,
 	go1_23_slices_zsortordered,
-	go1_23_slices_sort,
 	go1_23_slices_slices,
 }
