d1efee65f49b11f30dd0d00cb574becb359d47a2
pangdogs <pangdogs@gmail.com>
2025-01-08 19:17:13 +0800

    整理泛型库

--- cmd/yaegi/run.go
+++ cmd/yaegi/run.go
@@ -3,16 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/pangdogs/yaegi/interp"
-	"github.com/pangdogs/yaegi/stdlib"
-	"github.com/pangdogs/yaegi/stdlib/syscall"
-	"github.com/pangdogs/yaegi/stdlib/unrestricted"
-	"github.com/pangdogs/yaegi/stdlib/unsafe"
 	"go/build"
 	"os"
 	"reflect"
 	"strconv"
 	"strings"
+
+	"github.com/pangdogs/yaegi/interp"
+	"github.com/pangdogs/yaegi/stdlib"
+	"github.com/pangdogs/yaegi/stdlib/syscall"
+	"github.com/pangdogs/yaegi/stdlib/unrestricted"
+	"github.com/pangdogs/yaegi/stdlib/unsafe"
 )
 
 func run(arg []string) error {
@@ -25,14 +26,14 @@ func run(arg []string) error {
 	// The following flags are initialized from environment.
 	useSyscall, _ := strconv.ParseBool(os.Getenv("YAEGI_SYSCALL"))
 	useUnrestricted, _ := strconv.ParseBool(os.Getenv("YAEGI_UNRESTRICTED"))
-	//useUnsafe, _ := strconv.ParseBool(os.Getenv("YAEGI_UNSAFE"))
+	useUnsafe, _ := strconv.ParseBool(os.Getenv("YAEGI_UNSAFE"))
 
 	rflag := flag.NewFlagSet("run", flag.ContinueOnError)
 	rflag.BoolVar(&interactive, "i", false, "start an interactive REPL")
 	rflag.BoolVar(&useSyscall, "syscall", useSyscall, "include syscall symbols")
 	rflag.BoolVar(&useUnrestricted, "unrestricted", useUnrestricted, "include unrestricted symbols")
 	rflag.StringVar(&tags, "tags", "", "set a list of build tags")
-	//rflag.BoolVar(&useUnsafe, "unsafe", useUnsafe, "include unsafe symbols")
+	rflag.BoolVar(&useUnsafe, "unsafe", useUnsafe, "include unsafe symbols")
 	rflag.BoolVar(&noAutoImport, "noautoimport", false, "do not auto import pre-compiled packages. Import names that would result in collisions (e.g. rand from crypto/rand and rand from math/rand) are automatically renamed (crypto_rand and math_rand)")
 	rflag.StringVar(&cmd, "e", "", "set the command to be executed (instead of script or/and shell)")
 	rflag.Usage = func() {
@@ -51,6 +52,12 @@ func run(arg []string) error {
 		Env:          os.Environ(),
 		Unrestricted: useUnrestricted,
 	})
+	if err := i.Use(stdlib.Symbols); err != nil {
+		return err
+	}
+	if err := i.Use(interp.Symbols); err != nil {
+		return err
+	}
 	if useSyscall {
 		if err := i.Use(syscall.Symbols); err != nil {
 			return err
@@ -60,14 +67,14 @@ func run(arg []string) error {
 			return err
 		}
 	}
-	//if useUnsafe {
-	if err := i.Use(unsafe.Symbols); err != nil {
-		return err
+	if useUnsafe {
+		if err := i.Use(unsafe.Symbols); err != nil {
+			return err
+		}
+		if err := os.Setenv("YAEGI_UNSAFE", "1"); err != nil {
+			return err
+		}
 	}
-	//if err := os.Setenv("YAEGI_UNSAFE", "1"); err != nil {
-	//	return err
-	//}
-	//
 	if useUnrestricted {
 		// Use of unrestricted symbols should always follow stdlib and syscall symbols, to update them.
 		if err := i.Use(unrestricted.Symbols); err != nil {
@@ -76,12 +83,6 @@ func run(arg []string) error {
 		if err := os.Setenv("YAEGI_UNRESTRICTED", "1"); err != nil {
 			return err
 		}
-	}
-	if err := i.Use(stdlib.Symbols); err != nil {
-		return err
-	}
-	if err := i.Use(interp.Symbols); err != nil {
-		return err
 	}
 
 	if cmd != "" {
--- stdlib/generic/go1_23_generic.go
+++ stdlib/generic/go1_23_generic.go
@@ -7,50 +7,38 @@ import (
 	_ "embed"
 )
 
-//go:embed go1_23_cmp_cmp.go.txt
-var cmpSource string
-
-////go:embed go1_23_iter_iter.go.txt
-//var iterSource string
-
 //go:embed go1_23_maps_maps.go.txt
-var mapsSource string
+var go1_23_maps_maps string
 
-//go:embed go1_23_slices_slices.go.txt
-var slicesSource string
+//go:embed go1_23_slices_math_bits.go.txt
+var go1_23_slices_math_bits string
 
-//go:embed go1_23_slices_sort.go.txt
-var slicesSortSource string
+//go:embed go1_23_slices_cmp.go.txt
+var go1_23_slices_cmp string
+
+//go:embed go1_23_slices_iter.go.txt
+var go1_23_slices_iter string
 
 //go:embed go1_23_slices_zsortanyfunc.go.txt
-var slicesZsortanyfuncSource string
+var go1_23_slices_zsortanyfunc string
 
 //go:embed go1_23_slices_zsortordered.go.txt
-var slicesZsortorderedSource string
+var go1_23_slices_zsortordered string
 
-////go:embed go1_23_sync_atomic_type.go.txt
-//var syncAtomicTypeSource string
+//go:embed go1_23_slices_sort.go.txt
+var go1_23_slices_sort string
 
-//go:embed go1_23_sync_oncefunc.go.txt
-var syncOncefuncSource string
-
-////go:embed go1_23_unique_handle.go.txt
-//var uniqueHandle string
-//
-////go:embed go1_23_unique_clone.go.txt
-//var uniqueClone string
+//go:embed go1_23_slices_slices.go.txt
+var go1_23_slices_slices string
 
 // Sources contains the list of generic packages source strings.
 var Sources = [...]string{
-	cmpSource,
-	//iterSource,
-	mapsSource,
-	slicesZsortanyfuncSource,
-	slicesZsortorderedSource,
-	slicesSortSource,
-	slicesSource,
-	//syncAtomicTypeSource,
-	syncOncefuncSource,
-	//uniqueHandle,
-	//uniqueClone,
+	go1_23_maps_maps,
+	go1_23_slices_math_bits,
+	go1_23_slices_cmp,
+	go1_23_slices_iter,
+	go1_23_slices_zsortanyfunc,
+	go1_23_slices_zsortordered,
+	go1_23_slices_sort,
+	go1_23_slices_slices,
 }
